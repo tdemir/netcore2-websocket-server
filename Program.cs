2ebe46cc563a0470a1df012e45d89f056e76ef65
tdemir <td.demir@outlook.com>
2019-09-22 14:02:31 +0300

    default files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore2_websocket_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
